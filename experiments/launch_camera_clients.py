33e61f6aaf62ebac01815cd7ba6fc4768d0aeca2
Alberto Lyons <alberto.lyons@alumnos.ucn.cl>
2026-02-23 15:22:00 -0300

    docs: documented experiments folder

--- experiments/launch_camera_clients.py
+++ experiments/launch_camera_clients.py
@@ -1,45 +1,64 @@
+# Importación de bibliotecas necesarias
 from dataclasses import dataclass
 from typing import Tuple
-import numpy as np
 import tyro
 import cv2
+# Importa la clase ZMQClientCamera desde el módulo camera_node
 from gello.zmq_core.camera_node import ZMQClientCamera
-
+"""
+launch_camera_clients.py
+Este script lanza clientes de cámara que se conectan a servidores de cámara a través de ZMQ 
+lanzados desde launch_camera_nodes.py.
+Parametros asignados a través de la línea de comandos:
+- ports: Tupla de puertos a los que se conectarán los clientes
+- hostname: Dirección IP del host al que se conectarán los clientes
+"""
 @dataclass
 class Args:
     ports: Tuple[int, ...] = (5000,) 
     hostname: str = "127.0.0.1"
     # hostname: str = 192.168.53.152" 
-
+"""
+Función principal que inicia los clientes de cámara de Intel Realsense, se conecta a los servidores de cámara y muestra las las cámaras en separados
+"""
 def main(args):
+    # Inicializa los clientes de cámara y las ventanas de visualización
     cameras = []
     images_display_names = []
-    
+    # Recorre los puertos especificados en los argumentos
     for port in args.ports:
+        # Crea un cliente de cámara para cada puerto y lo agrega a la lista de cámaras
         cameras.append(ZMQClientCamera(port=port, host=args.hostname))
         images_display_names.append(f"Camera_Port_{port}")
+        # Crea una ventana de visualización para cada cámara
         cv2.namedWindow(f"Camera_Port_{port}", cv2.WINDOW_NORMAL)
-
     print("Client started.")
-
+    # Bucle principal para leer las imágenes de las cámaras y mostrarlas en cada ventana la imagen a color, y la imagen de profundidad
     while True:
+        # Recorre cada cámara y su correspondiente nombre de visualización
         for display_name, camera in zip(images_display_names, cameras):
+            # Lee la imagen y la profundidad de la cámara
             result = camera.read()
-            
+            # Si no se obtiene ningún resultado, continúa con la siguiente iteración
             if result is None:
                 continue
+            # Desempaqueta la imagen y la profundidad del resultado
             image, depth = result
+            # Convierte la imagen de RGB a BGR para su visualización con OpenCV
             image_bgr = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-            
+            # Aplica un mapa de colores a la imagen de profundidad para su visualización
             depth_colormap = cv2.applyColorMap(
                 cv2.convertScaleAbs(depth, alpha=0.03), 
                 cv2.COLORMAP_JET
             )
+            # Combina la imagen de la cámara y el mapa de colores de profundidad en una sola imagen para mostrar
             canvas = cv2.hconcat([image_bgr, depth_colormap])
+            # Muestra la imagen combinada en la ventana correspondiente
             cv2.imshow(display_name, canvas)
+        # Si se presiona la tecla 'q', sale del bucle y cierra las ventanas
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-
+    # Cierra todas las ventanas de visualización
     cv2.destroyAllWindows()
 
 if __name__ == "__main__":
--- experiments/launch_nodes.py
+++ experiments/launch_nodes.py
@@ -1,20 +1,31 @@
+# Importación de librerías
 from dataclasses import dataclass
 from pathlib import Path
-
 import tyro
+# Importación de la clase ZMQServerRobot desde el módulo robot_node
 from gello.zmq_core.robot_node import ZMQServerRobot
-
-
+"""
+launch_nodes.py
+Script para lanzar el servidor del robot, que puede ser un robot simulado o un robot físico, dependiendo de los argumentos proporcionados. 
+El servidor se comunica a través de ZMQ que permite controlar el robot desde otros procesos.
+Parámetros asignados desde consola:
+- robot: tipo de robot a lanzar (xarm, xarm_no_arm, sim_xarm, sim_xarm_no_arm)
+- robot_port: puerto en el que el servidor del robot escuchará las conexiones
+- hostname: dirección IP en la que el servidor del robot estará disponible
+- robot_ip: dirección IP del robot físico
+"""
 @dataclass
 class Args:
     robot: str = "xarm"
     robot_port: int = 6001
     hostname: str = "127.0.0.1"
-    robot_ip: str = "192.168.123.52"
-
-
+    robot_ip: str = "192.168.123.52" # IP del robot xArm5 físico en la red IoT_v3
+"""
+Función que lanza el servidor del robot dependiendo del tipo de robot especificado en los argumentos.
+"""
 def launch_robot_server(args: Args):
     port = args.robot_port
+    # Lanza un servidor del robot xArm5 simulado utilizando MuJoCo
     if args.robot == "sim_xarm":
         from gello.robots.sim_robot import MujocoRobotServer
 
@@ -26,7 +37,9 @@ def launch_robot_server(args: Args):
         server = MujocoRobotServer(
             xml_path=xml, gripper_xml_path=gripper_xml, port=port, host=args.hostname
         )
+        # Inicia el servidor del robot simulado
         server.serve()
+    # Lanza un servidor del robot xArm5 simulado sin brazo utilizando MuJoCo
     elif args.robot == "sim_xarm_no_arm":
         from gello.robots.sim_robot import MujocoRobotServer
 
@@ -38,12 +51,15 @@ def launch_robot_server(args: Args):
         server = MujocoRobotServer(
             xml_path=xml, gripper_xml_path=gripper_xml, port=port, host=args.hostname
         )
+        # Inicia el servidor del robot simulado
         server.serve()
-
+    # Lanza un servidor del robot xArm5 físico
     else:
+        # Lanza el servidor del robot con agarre
         if args.robot == "xarm":
             from gello.robots.xarm_robot import XArmRobot
             robot = XArmRobot(ip=args.robot_ip)
+        # Lanza el servidor del robot sin agarre
         elif args.robot == "xarm_no_arm":
             from gello.robots.xarm_robot_no_arm import XArmRobot_NoArm
             robot = XArmRobot_NoArm(ip=args.robot_ip)
@@ -51,11 +67,10 @@ def launch_robot_server(args: Args):
             raise NotImplementedError(
                 f"Robot {args.robot} not implemented, choose one of: xarm, xarm_no_arm, sim_xarm, sim_xarm_no_arm"
             )
+        # Inicia el servidor del robot físico con ZMQ
         server = ZMQServerRobot(robot, port=port, host=args.hostname)
         print(f"Starting robot server on port {port}")
         server.serve()
-
-
 
 def main(args):
     launch_robot_server(args)
